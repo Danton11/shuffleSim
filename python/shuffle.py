1c06be0d1eb322f58dfc77026cb479287e40d857
Daniel Benton <dxb834@student.bham.ac.uk>
2023-01-31 16:46:05 +0000

    Added some final comments

--- python/shuffle.py
+++ python/shuffle.py
@@ -1,19 +1,19 @@
-
 import random
 import unittest
 
-class Card: # card object with a value and suit, i.e (3,hearts)
+
+class Card:  # card object with a value and suit, i.e (3,hearts)
     def __init__(self, value, suit):
-        match value: # switch statement for card generation
-            case 1: # if an ace
-                self.value = 'Ace'
+        match value:  # switch statement for card generation
+            case 1:  # if an ace
+                self.value = "Ace"
             case 11:
-                self.value = 'Jack'
+                self.value = "Jack"
             case 12:
-                self.value = 'Queen'
+                self.value = "Queen"
             case 13:
-                self.value = 'King'
-            case _:
+                self.value = "King"
+            case _:  # default for non-face card value
                 self.value = value
         self.suit = suit
 
@@ -25,8 +25,8 @@ def non_perfect_riffle_shuffle(deck):
     :return: list of elements representing a shuffled deck of cards
     """
     # Split the deck into two halves
-    deck1 = deck[:len(deck)//2]
-    deck2 = deck[len(deck)//2:]
+    deck1 = deck[: len(deck) // 2]  # list splicing
+    deck2 = deck[len(deck) // 2 :]
 
     # Initialize the shuffled deck
     shuffled_deck = []
@@ -54,49 +54,62 @@ def perfect_riffle_shuffle(deck):
     middle_point = len(deck) // 2
 
     # Split the deck into two halves
-    deck, second_deck = deck[:middle_point], deck[middle_point:] # first card up to middle point, middle to the last card
+    deck, second_deck = (
+        deck[:middle_point],
+        deck[middle_point:],
+    )  # first card up to middle point, middle to the last card
 
     # Take elements from the second deck and add them to the correct place in the first deck
     for index, item in enumerate(second_deck):
 
         # finds the index in the other half of the deck and adds 1 to find where to insert
-        insert_index = index*2 + 1 
-        
+        insert_index = index * 2 + 1
+
         deck.insert(insert_index, item)
     return deck
 
 
-
-
-
-if __name__ == '__main__':
-    suits = ['hearts', 'diamonds', 'spades', 'clubs']
-    ordered_deck = [Card(value, suit) for value in range(1, 14) for suit in suits] # list comp to generate a list of Card objects with a range of values
+if __name__ == "__main__":
+    suits = ["hearts", "diamonds", "spades", "clubs"]
+    ordered_deck = [
+        Card(value, suit) for value in range(1, 14) for suit in suits
+    ]  # list comp to generate a list of Card objects with a range of values
 
     print("Perfect riffle shuffle:")
 
-    #output the deck following the perfect riffle shuffle
-    for card in (perfect_riffle_shuffle(ordered_deck)):
+    # output the deck following the perfect riffle shuffle
+    for card in perfect_riffle_shuffle(ordered_deck):
         print(f"{card.value} of {card.suit}")
 
     print("Non-Perfect riffle shuffle:")
     # output the deck following the non-perfect riffle shuffle
-    for card in (non_perfect_riffle_shuffle(ordered_deck)):
+    for card in non_perfect_riffle_shuffle(ordered_deck):
         print(f"{card.value} of {card.suit}")
 
 
+# Testing
 
-
-
-# Testing 
 
 class RiffleTestCase(unittest.TestCase):
     def test_perfect_riffle_shuffle(self):
         """
         Tests the perfect_riffle_shuffle function with a simple deck of cards.
         """
-        riffleEight = perfect_riffle_shuffle(perfect_riffle_shuffle(perfect_riffle_shuffle(perfect_riffle_shuffle(
-            perfect_riffle_shuffle(perfect_riffle_shuffle(perfect_riffle_shuffle(perfect_riffle_shuffle(ordered_deck))))))))
+        riffleEight = perfect_riffle_shuffle(
+            perfect_riffle_shuffle(
+                perfect_riffle_shuffle(
+                    perfect_riffle_shuffle(
+                        perfect_riffle_shuffle(
+                            perfect_riffle_shuffle(
+                                perfect_riffle_shuffle(
+                                    perfect_riffle_shuffle(ordered_deck)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
         # eight perfect riffle shuffles on any deck generates the oringal deck
         self.assertEqual(ordered_deck, riffleEight)
 
@@ -113,9 +126,9 @@ class RiffleTestCase(unittest.TestCase):
         self.assertEqual(len(deck), len(shuffled_deck))
 
         # Assert that the shuffled deck satisfies the perfect riffle shuffle pattern
-        for i in range(len(deck)//2):
-            self.assertEqual(shuffled_deck[2*i], deck[i])
-            self.assertEqual(shuffled_deck[2*i + 1], deck[i + len(deck)//2])
+        for i in range(len(deck) // 2):
+            self.assertEqual(shuffled_deck[2 * i], deck[i])
+            self.assertEqual(shuffled_deck[2 * i + 1], deck[i + len(deck) // 2])
 
     def test_non_perfect_riffle_shuffle(self):
         """
@@ -135,7 +148,6 @@ class RiffleTestCase(unittest.TestCase):
 
         # Assert that all elements in the original deck are in the shuffled deck and vice versa
         self.assertCountEqual(deck, shuffled_deck)
-
 
 
 unittest.main()
